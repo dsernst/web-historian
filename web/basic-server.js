b697f653d2cd5af1684f5827869953e78d3e39fe
dsernst <github@dsernst.com>
2015-01-07 15:44:48 -0800

    pass first test

--- web/basic-server.js
+++ web/basic-server.js
@@ -6,7 +6,7 @@ var initialize = require("./initialize.js");
 // HINT:It has to do with what's in .gitignore
 initialize();
 
-var port = 8080;
+var port = 3000;
 var ip = "127.0.0.1";
 var server = http.createServer(handler.handleRequest);
 console.log("Listening on http://" + ip + ":" + port);
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,35 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
-};
+  console.log("Serving " + req.url + " with method " + req.method)
+  var publicPath = './web/public'
+
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      fs.readFile(publicPath + '/index.html', function(err, data) {
+        if (err) {
+          throw err
+        } else {
+          res.writeHead(200)
+          res.end(data)
+        }
+      })
+    } else if (req.url === '/styles.css') {
+      fs.readFile(publicPath + '/styles.css', function(err, data) {
+        if (err) {
+          throw err
+        } else {
+          res.writeHead(200)
+          res.end(data)
+        }
+      })
+    } else {
+      res.writeHead(404)
+      res.end()
+    }
+  }
+  // res.end(archive.paths.list);
+}
