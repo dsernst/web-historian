b3e54cf702f3855c2afb76a782e10d1cad844439
dsernst <github@dsernst.com>
2015-01-07 16:39:36 -0800

    better 404 handling

--- web/request-handler.js
+++ web/request-handler.js
@@ -6,13 +6,22 @@ var post = require('./routes.js').post
 exports.handleRequest = function (req, res) {
   console.log(req.method + " " + req.url)
 
+  var return404 = function () {
+    console.log('-> sending those fools a 404')
+    res.writeHead(404)
+    res.end()
+  }
+
   if (req.method === 'GET') {
-    get[req.url](req, res)
+    if (get.hasOwnProperty(req.url)) {
+      get[req.url](req, res)
+    } else {
+      return404()
+    }
   } else if (req.method === 'POST') {
     post[req.url](req, res)
   } else {
-    res.writeHead(404)
-    res.end()
+    return404()
   }
 
 }
